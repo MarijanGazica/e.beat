e349dc1ca1a4699f9fdd62fe65eb42b540101db4
Marijan <marijan.gazica@gmail.com>
2018-10-23 21:16:11 +0200

    added room dependancy, migrated to androidx

--- app/src/main/java/studio/nodroid/bloodpressurehelper/ui/MainActivity.kt
+++ app/src/main/java/studio/nodroid/bloodpressurehelper/ui/MainActivity.kt
@@ -1,7 +1,7 @@
 package studio.nodroid.bloodpressurehelper.ui
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
--- app/src/main/java/studio/nodroid/bloodpressurehelper/ui/inputHistory/InputHistoryFragment.kt
+++ app/src/main/java/studio/nodroid/bloodpressurehelper/ui/inputHistory/InputHistoryFragment.kt
@@ -1,7 +1,7 @@
 package studio.nodroid.bloodpressurehelper.ui.inputHistory
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
