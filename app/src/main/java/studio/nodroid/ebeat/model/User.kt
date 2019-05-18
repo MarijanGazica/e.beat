9673729deb04af8fa361e04592dd06604249447b
Marijan <marijan.gazica@gmail.com>
2019-05-18 19:51:11 +0200

    list visual update, dependency update

--- app/src/main/java/studio/nodroid/ebeat/model/User.kt
+++ app/src/main/java/studio/nodroid/ebeat/model/User.kt
@@ -1,8 +1,10 @@
 package studio.nodroid.ebeat.model
 
+import androidx.annotation.Keep
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Keep
 @Entity
 data class User(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
