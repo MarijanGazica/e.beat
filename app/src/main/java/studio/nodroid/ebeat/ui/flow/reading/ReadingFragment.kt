f7cde965c9aac0ffaf045ff2b4877636bdca6236
Marijan <marijan.gazica@gmail.com>
2019-06-24 20:36:13 +0200

    dep bump add reading view transitions

--- app/src/main/java/studio/nodroid/ebeat/ui/flow/reading/ReadingFragment.kt
+++ app/src/main/java/studio/nodroid/ebeat/ui/flow/reading/ReadingFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_reading.*
 import studio.nodroid.ebeat.R
@@ -14,6 +15,8 @@ class ReadingFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_reading, container, false)
     }
+
+    var step = 0
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -28,8 +31,42 @@ class ReadingFragment : Fragment() {
 
 
         icon.setOnClickListener {
-            motionLayout.transitionToState(R.id.userSet)
+            Toast.makeText(requireContext(), "$step", Toast.LENGTH_SHORT).show()
+            when (step) {
+                0 -> {
+                    motionLayout.transitionToState(R.id.userSet)
+                    step++
+                }
+                1 -> {
+                    motionLayout.transitionToState(R.id.dateSet)
+                    step++
+                }
+                2 -> {
+                    motionLayout.transitionToState(R.id.timeSet)
+                    step++
+                }
+                3 -> {
+                    motionLayout.transitionToState(R.id.systolicSet)
+                    step++
+                }
+                4 -> {
+                    motionLayout.transitionToState(R.id.diastolicSet)
+                    step++
+                }
+                5 -> {
+                    motionLayout.transitionToState(R.id.pulseSet)
+                    step++
+                }
+                6 -> {
+                    motionLayout.transitionToState(R.id.descriptionSet)
+                    step++
+                }
+                else -> {
+                    motionLayout.transitionToState(R.id.start)
+                    step = 0
+                }
+            }
         }
-    }
 
+    }
 }
